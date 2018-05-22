a79e59dfeddab5113ee4b4917ab156e60887151b
magnuspl <magnuslangeland80@gmail.com>
2018-05-22 16:46:18 +0200

    db host change

--- ntnui/settings.py
+++ ntnui/settings.py
@@ -104,7 +104,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
         'PORT': 5432,
     }
 }
