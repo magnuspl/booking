8177e51fea276cc6214bbf7b732885f33b4174c3
magnuspl <magnuslangeland80@gmail.com>
2018-05-23 14:15:58 +0200

    more heroku attempts

--- ntnui/settings.py
+++ ntnui/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'ntnui.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = dj_database_url.config()
 
 AUTH_USER_MODEL = "accounts.User"
 AUTH_GROUPIMAGE_MODEL = "groups.GroupImage"
