827dffb6f1f43f0d9b07c311e17d424863230e5b
magnuspl <magnuslangeland80@gmail.com>
2018-05-23 14:26:44 +0200

    change os env

--- ntnui/settings.py
+++ ntnui/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'ntnui.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
